2cd23f1b5139954647fb3af7ca4cbba6cd544fb8
canisterism <ohtthstc@gmail.com>
2020-02-24 17:08:22 +0900

    :+1: create simple layout

--- lib/main.dart
+++ lib/main.dart
@@ -10,31 +10,56 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MainPage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({this.title});
+class MainPage extends StatefulWidget {
+  const MainPage({Key key}) : super(key: key);
 
-  final String title;
+  @override
+  _MainPageState createState() => _MainPageState();
+}
 
+class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(title),
+        title: Text('Search Screen'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+            FlatButton(
+              onPressed: () => print('tapped'),
+              child: Container(
+                width: 200,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Center(
+                    child: Text(
+                  'next screen',
+                  style: TextStyle(fontSize: 24, color: Colors.white),
+                )),
+              ),
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              title: Text('Search'), icon: Icon(Icons.search)),
+          BottomNavigationBarItem(
+              title: Text('Account'), icon: Icon(Icons.account_circle)),
+        ],
+        selectedItemColor: Colors.amber[800],
       ),
     );
   }
